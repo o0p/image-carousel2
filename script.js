41e3ed2e5a9478f5533d230430f389d1fa3a30b9
Russell <russelldev@protonmail.com>
2021-02-18 08:07:06 +0000

    Add selector vars

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getAnimations('left');
+const rightBtn = document.getAnimations('right');
+const img = document.querySelectorAll('#imgs img');
+let idx = 0;
+let interval = setInterval(run, 2000);
