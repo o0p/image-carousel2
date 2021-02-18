60e258a3ecc216a6ea975c55ace7b69143432f5f
Russell <russelldev@protonmail.com>
2021-02-18 08:26:19 +0000

    Add resetInterval()

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const imgs = document.getElementById('imgs');
-const leftBtn = document.getAnimations('left');
-const rightBtn = document.getAnimations('right');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
 const img = document.querySelectorAll('#imgs img');
 let idx = 0;
 let interval = setInterval(run, 2000);
@@ -17,4 +17,9 @@ function changeImage() {
         idx = img.length - 1;
     };
     imgs.style.transform = `translateX(${-idx * 500}px)`
+};
+
+function resetInterva() {
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
 };
